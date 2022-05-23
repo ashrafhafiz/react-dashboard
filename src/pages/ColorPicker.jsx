7a7f0700579ab4d9b0edae8f0a43764762e4ec24
Ashraf Hafiz <Ashraf.Hafiz@yahoo.com>
2022-05-23 22:24:43 +0200

    23052022-10:30PM

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 import { ColorPickerComponent } from "@syncfusion/ej2-react-inputs";
+import {
+  AccordionComponent,
+  AccordionItemDirective,
+  AccordionItemsDirective,
+} from "@syncfusion/ej2-react-navigations";
+import { ListViewComponent } from "@syncfusion/ej2-react-lists";
 
 import { Header } from "../components";
 
@@ -8,7 +14,36 @@ const changeColor = (args) => {
     args.currentValue.hex;
 };
 
+const content0 = () => {
+  return (
+    <div>
+      Microsoft ASP.NET is a set of technologies in the Microsoft .NET Framework
+      for building Web applications and XML Web services.
+    </div>
+  );
+};
+const content1 = () => {
+  return (
+    <div>
+      The Model-View-Controller (MVC) architectural pattern separates an
+      application into three main components: the model, the view, and the
+      controller.
+    </div>
+  );
+};
+const content2 = () => {
+  return (
+    <div>
+      JavaScript (JS) is an interpreted computer programming language.It was
+      originally implemented as part of web browsers so that client-side scripts
+      could interact with the user, control the browser, communicate
+      asynchronously, and alter the document content that was displayed.
+    </div>
+  );
+};
 const ColorPicker = () => {
+  const data = ["فنون", "ملخصات", "رسومات", "سيراميك", "كرتون", "لوحات"];
+
   return (
     <div className="m-10 p-10 bg-white rounded-3xl">
       <Header title="Color Picker" category="Application" />
@@ -37,6 +72,30 @@ const ColorPicker = () => {
               change={changeColor}
             ></ColorPickerComponent>
           </div>
+        </div>
+      </div>
+      <div className="m-10 p-10">
+        <AccordionComponent>
+          <AccordionItemsDirective>
+            <AccordionItemDirective
+              expanded="true"
+              header="ASP.NET"
+              content={content0}
+            />
+            <AccordionItemDirective header="ASP.NET MVC" content={content1} />
+            <AccordionItemDirective header="JavaScript" content={content2} />
+          </AccordionItemsDirective>
+        </AccordionComponent>
+      </div>
+      <div className="flex justify-center items-center m-10 p-10">
+        <div className="w-72">
+          <ListViewComponent
+            id="list"
+            dataSource={data}
+            showHeader="true"
+            headerTitle="أنواع الفنون المتاحة RTL"
+            enableRtl="true"
+          ></ListViewComponent>
         </div>
       </div>
     </div>
